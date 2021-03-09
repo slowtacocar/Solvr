927cb7f0378048620d9e0933ffc3c1400053d8c0
Bobby George <slowtacocar@gmail.com>
2021-03-08 22:59:31 -0500

    Fix filename case issues

---
+++ Solvr/Solvr.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "Addition.h"
+#include "Subtraction.h"
+#include "Multiplication.h"
+#include "Division.h"
+#include "Exponentiation.h"
+#include "Variable.h"
+#include "BadExpression.h"
+
+#include <iostream>
